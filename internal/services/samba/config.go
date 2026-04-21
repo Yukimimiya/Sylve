41b930af8599c3f23d0d49db096a7c769e1bb14a
Takeshi Taguchi <taguchi949@gmail.com>
2026-04-21 18:31:05 +0900

    add my local options

--- internal/services/samba/config.go
+++ internal/services/samba/config.go
@@ -587,8 +587,10 @@ func (s *Service) ShareConfig(ctx context.Context) (string, error) {
 			}
 		}
 		config.WriteString("\tfull_audit:prefix = sylve-smb-al|%u|%I|%m|%S|%P\n")
-		config.WriteString("\tfull_audit:success = openat close read write renameat unlinkat mkdirat create_file connect disconnect\n")
-		config.WriteString("\tfull_audit:failure = all !getwd !get_real_filename !fgetxattr !fget_dos_attributes\n")
+		// config.WriteString("\tfull_audit:success = openat close read write renameat unlinkat mkdirat create_file connect disconnect\n")
+		// config.WriteString("\tfull_audit:failure = all !getwd !get_real_filename !fgetxattr !fget_dos_attributes\n")
+		config.WriteString("\tfull_audit:success = open, opendir, rename, unlink\n")
+		config.WriteString("\tfull_audit:failure = none\n")
 		config.WriteString("\tfull_audit:facility = LOCAL7\n")
 		config.WriteString("\tfull_audit:priority = ALERT\n")
 		config.WriteString("\tfull_audit:syslog = true\n")
@@ -667,14 +669,34 @@ func (s *Service) WriteConfig(ctx context.Context, reload bool) error {
 	fullConfig += "\tdirectory mode = 0744\n"
 	fullConfig += "\tvalid users = %S\n"
 	fullConfig += "\tfull_audit:prefix = sylve-smb-homes-al|%u|%I|%m|%S|%P\n"
-	fullConfig += "\tfull_audit:success = openat close read write renameat unlinkat mkdirat create_file connect disconnect\n"
-	fullConfig += "\tfull_audit:failure = all !getwd !get_real_filename !fgetxattr !fget_dos_attributes\n"
+	// fullConfig += "\tfull_audit:success = openat close read write renameat unlinkat mkdirat create_file connect disconnect\n"
+	// fullConfig += "\tfull_audit:failure = all !getwd !get_real_filename !fgetxattr !fget_dos_attributes\n"
+	fullConfig += "\tfull_audit:success = open, opendir, rename, unlink\n"
+	fullConfig += "\tfull_audit:failure = none\n"
 	fullConfig += "\tfull_audit:facility = LOCAL7\n"
 	fullConfig += "\tfull_audit:priority = ALERT\n"
 	fullConfig += "\tfull_audit:syslog = true\n"
 	fullConfig += "\tfull_audit:log_secdesc = true\n"
 
 	filePath := "/usr/local/etc/smb4.conf"
+	fullConfig += "\n"
+	fullConfig += "[homes]\n"
+	fullConfig += "\tcomment = Home Directories\n"
+	fullConfig += "\tbrowseable = no\n"
+	fullConfig += "\tread only = no\n"
+	fullConfig += "\tcreate mode = 0644\n"
+	fullConfig += "\tdirectory mode = 0744\n"
+	fullConfig += "\tvalid users = %S\n"
+	fullConfig += "\tfull_audit:prefix = sylve-smb-homes-al|%u|%I|%m|%S|%P\n"
+	// fullConfig += "\tfull_audit:success = openat close read write renameat unlinkat mkdirat create_file connect disconnect\n"
+	// fullConfig += "\tfull_audit:failure = all !getwd !get_real_filename !fgetxattr !fget_dos_attributes\n"
+	fullConfig += "\tfull_audit:success = open, opendir, rename, unlink\n"
+	fullConfig += "\tfull_audit:failure = none\n"
+	fullConfig += "\tfull_audit:facility = LOCAL7\n"
+	fullConfig += "\tfull_audit:priority = ALERT\n"
+	fullConfig += "\tfull_audit:syslog = true\n"
+	fullConfig += "\tfull_audit:log_secdesc = true\n"
+	fullConfig += "\n"
 
 	if err := os.WriteFile(filePath, []byte(fullConfig), 0644); err != nil {
 		return fmt.Errorf("failed to write Samba configuration to %s: %w", filePath, err)
